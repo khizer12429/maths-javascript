53576594ac95e4b3bc7478398a194ba4fc332e9b
Khizer Farooq <khizerfarooq267@gmail.com>
2024-12-20 23:41:38 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,224 @@
+// chapter 4
+
+// question no 1
+// let name = " khizer" ; let age =  12 ; let institute = " SMIT";
+
+// question no 2
+
+// legal variable name
+// let name = " khizer" ; 
+// let age = 12 ; 
+// let institute = " SMIT" ; 
+// let last_name = "farooq";
+// let qualification_name = "cs";
+
+// illegal variable name
+// let 12name="khizer";
+// let @first_name="farooq";
+// let #age = 12 ;
+// let @last_name="farooq";
+// let 8name8= "far";
+
+
+// question no 3
+// b
+//  let $my_1stVariable= "khizer";
+//  let my_2ndVariable= 12;
+//  let my_3rdVariable= "SMIT";
+//  let my_4last_name= "farooq";
+// c
+// let $name = "khizer";
+//  let _age = 12;
+//  let institute = "SMIT";
+// d
+// let name = "khizer";
+// let Name = "farooq";
+
+// console.log(name);  
+// console.log(Name);  
+// e
+// let if = "khizer";
+// let var = "SMIT";
+
+
+// chapter 5
+// question 1
+// let a = 2;
+// let b = 3;
+// let c= a + b;
+
+// document.write("sum of 2 and 3 is" + c);
+
+// question 2
+// for subtraction
+// let a = 2;
+// let b = 3;
+// let c= a - b;
+
+// document.write("subtraction of 2 and 3 is" + c);
+
+// for multiplication
+// let a = 2;
+// let b = 3;
+// let c= a * b;
+
+// document.write("multiplication of 2 and 3 is" + c);
+
+// for modulas
+// let a = 2;
+// let b = 3;
+// let c= a % b;
+
+// document.write("modulas of 2 and 3 is" + c);
+
+// question 3
+// let a ;
+// document.write("Value after variable declaration is undefined<br>");
+// let b = 5;
+// document.write("initial value is:"+ b +"<br>");
+// let c = b + 1;
+// document.write("value after increament is :" + c +"<br>" );
+// let d = b + 8;
+// document.write("value after increament is :" + d +"<br>" );
+// let e = b - 1;
+// document.write("value after increament is :" + e +"<br>" );
+// document.write("remainder is : 0" +"<br>" );
+
+// question 4
+// let a = 600;
+// let b = a * 5;
+
+// document.write("Total Cost To Buy 5 ticket to a movie is :" + b +"PKR" + "<br>");
+
+// question 5
+// let a = 4;
+// document.write("table of 4<br>");
+// document.write("4 x 1 ="+a*1+"<br>");
+// document.write("4 x 2 ="+a*2+"<br>");
+// document.write("4 x 3 ="+a*3+"<br>");
+// document.write("4 x 4 ="+a*4+"<br>");
+// document.write("4 x 5 ="+a*5+"<br>");
+// document.write("4 x 6 ="+a*6+"<br>");
+// document.write("4 x 7 ="+a*7+"<br>");
+// document.write("4 x 8 ="+a*8+"<br>");
+// document.write("4 x 9 ="+a*9+"<br>");
+// document.write("4 x 10 ="+a*10+"<br>");
+
+// question 6
+// let f_temp = 25;
+// let c_temp = 30;
+// let c= (f_temp-32)*5/9;
+// let f= (c_temp*9/5)+32;
+// document.write("25`c is" + c + "<br>");
+// document.write("25`c is" + f);
+
+// question 7
+// let a = 650;
+// let b = 550;
+// let c = a*2 + 550*5
+
+// document.write("price of item 1 is 650" + "<br>");
+// document.write("price of item 2 is 550"+ "<br>");
+// document.write("ordered quantity of item 1 is 2"+ "<br>");
+// document.write("ordered quantity of item 2 is 5"+ "<br>");
+// document.write("shipping charges 100"+ "<br>");
+
+// document.write("total cost of your order is:" + c)
+
+// question 8
+// let total_marks = 980;
+// let marks_obtained = 804;
+// let percentage = (marks_obtained / total_marks)*100;  
+// document.write("your percentage is " + percentage + "%");
+
+// question 9
+// let us_dollar = 10;
+// let saudi_riyal = 25;
+// let total_currency = us_dollar*10 + saudi_riyal*25;
+
+// document.write("total currency in pakistani ruppe :" + total_currency);
+
+// question 10
+// let a = 10;
+// let b = (a+5)*(a*10)*(a/2);
+// document.write( b );
+
+// question 11
+// let current_year = 2016;
+// let birth_year =  1992;
+
+// let age = 2016-1992
+// document.write("your age is "+ age); 
+
+// question 12
+// let radius = 20;
+// let circumference = 2 * 3.14 * 20;
+// document.write("the circumference is "+ circumference + "<br>");
+// let area = 3.14 * (20 * 2);
+// document.write("the area is : "+ area);
+
+// question 13
+// let f_snack = "chocochip";
+// let current_age = 21;
+// let max_age = 50;
+// let snack_perday = 5;
+// let total_snack = (max_age - current_age) * snack_perday;
+
+// document.write("you will need " + total_snack + " chocochip to last you untill the ripe old age of " + max_age);
+
+
+// chapter 6-9
+// document.write("Result:"+"<br>");
+// document.write("The value  of a is : 10" +"<br>");
+// document.write("............................."+"<br>");
+
+// let a = 10;
+// document.write("The value  of ++a is :" +  ++a + "<br>");
+// document.write("now the value of  a  is :" + a + "<br>");
+// document.write("............................."+"<br>");
+// document.write("The value  of a++ is :" +  a++ + "<br>");
+// document.write("now the value of  a  is :" + a +"<br>");
+// document.write("............................."+"<br>");
+// document.write("The value  of --a is :" +  --a + "<br>");
+// document.write("now the value of  a  is :" + a +"<br>");
+// document.write("............................."+"<br>");
+// document.write("The value  of a-- is :" +  a-- + "<br>");
+// document.write("now the value of  a  is :" + a +"<br>");
+
+// question no 2
+// let a =  2;
+// let b = 1;
+
+// document.write("--a :" + --a + "<br>");
+// document.write("--a - --b:" + Number(1+0) + "<br>");
+// document.write("--a - --b + ++b:" + Number(1+0+1) + "<br>");
+// document.write("--a - --b + ++b + b--:" + Number(1+0+1+1) + "<br>");
+
+// question no 3
+// document.write(prompt());
+
+// question no 4
+
+// if (prompt()== 5){
+//     let a = 5;
+//     document.write("table of 5<br>");
+//     document.write("5 x 1 ="+a*1+"<br>");
+//     document.write("5 x 2 ="+a*2+"<br>");
+//     document.write("5 x 3 ="+a*3+"<br>");
+//     document.write("5 x 4 ="+a*4+"<br>");
+//     document.write("5 x 5 ="+a*5+"<br>");
+//     document.write("5 x 6 ="+a*6+"<br>");
+//     document.write("5 x 7 ="+a*7+"<br>");
+//     document.write("5 x 8 ="+a*8+"<br>");
+//     document.write("5 x 9 ="+a*9+"<br>");
+//     document.write("5 x 10 ="+a*10+"<br>");
+// }
+
+
+
+
+
+
+
+
+
